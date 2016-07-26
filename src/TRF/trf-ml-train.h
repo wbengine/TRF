fed397e23cac87f6f553f461eb80c3f1c610bba1
wbengine <wb.th08@gmail.com>
2016-07-26 11:03:43 +0800

    upload the TRF feature style files

--- src/TRF/trf-ml-train.h
+++ src/TRF/trf-ml-train.h
@@ -1,3 +1,20 @@
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright 2014-2015 Tsinghua University
+// Author: wb.th08@gmail.com (Bin Wang), ozj@tsinghua.edu.cn (Zhijian Ou) 
+//
+// All h, cpp, cc, and script files (e.g. bat, sh, pl, py) should include the above 
+// license declaration. Different coding language may use different comment styles.
+
+
 #pragma once
 #include "trf-model.h"
 #include "trf-corpus.h"
